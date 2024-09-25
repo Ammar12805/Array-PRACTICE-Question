856e781201a69aa823908fe12edaa8ef056e13ac
Ammar Ahmed <ammarahmed12805@gmail.com>
2024-09-25 19:09:57 +0500

    Two Files upload

---
+++ app.js
@@ -0,0 +1,237 @@
+// Chp (14-16)
+
+// Q1
+
+// var arr = [];
+
+// console.log(arr);
+
+
+// Q2
+
+// var arr = ["abc" , "xyz" , "thz" , "plm"];
+// console.log(arr)
+
+
+// Q3
+
+// var arr = [123 , 456 , 789];
+// console.log(arr);
+
+
+// Q4
+
+// var arr = [false , true];
+// console.log(arr);
+
+
+// Q5
+
+// var arr = [123 , 456 , 789];
+// console.log(arr);
+
+// Q6
+
+// var arr = [123 , "abc" , true , undefined];
+// console.log(arr);
+
+
+// Q7
+
+// var arr = ["SSC", "HSC", "BCS",
+//     "BS", "BCOM", "MS", "M. Phil", "PhD"]
+//     document.write("<h1>Qualifications</h1>")
+//     document.write( arr[0] +"<br>")
+//     document.write( arr[1] +"<br>")
+//     document.write( arr[2] +"<br>")
+//     document.write( arr[3] +"<br>")
+//     document.write( arr[4] +"<br>")
+//     document.write( arr[5] +"<br>")
+//     document.write( arr[6] +"<br>")
+//     document.write( arr[7] +"<br>")
+
+
+// Q8
+
+// var studentArr = ["Micheal" , "John" , "Tony"];
+// var marksArr = [320 , 230 , 480];
+// var totalMarks = 500;
+// var percentageArr = [marksArr[0]/totalMarks*100 , marksArr[1]/totalMarks*100 , marksArr[2]/totalMarks*100];
+// document.write(`Score of ${studentArr[0]} is ${marksArr[0]} . Percentage: ${percentageArr[0]}% <br>`)
+// document.write(`Score of ${studentArr[1]} is ${marksArr[1]} . Percentage: ${percentageArr[1]}% <br>`)
+// document.write(`Score of ${studentArr[2]} is ${marksArr[2]} . Percentage: ${percentageArr[2]}% <br>`)
+
+
+
+// Q9
+
+
+// var arr = [320 , 230 , 480 , 120];
+// document.write(`Scores of Students : ${arr} <br> `)
+// var updatedArr = arr.sort();
+// document.write  
+// (`Ordered Scores of Student : ${updatedArr}`)
+
+
+// Q10
+
+// var arr = ["Karachi", 'Lahore' , 'Islamabad' , 'Quetta' , 'Peshawar']
+// document.write(`Cities List: <br> ${arr} <br>`)
+
+// var selctedList = arr.slice(2,4);
+// document.write(`Selected Cities List: <br> ${selctedList}`)
+
+
+// Q11
+
+// var arr = ["This" , "is" , "my" , 'cat'];
+// document.write(arr + "<br>");
+
+// arr = arr.join("");
+// document.write(arr)
+
+
+// Q12
+
+// var arr = ["Apple" , 'Samsung' , 'Motorolla' , 'Nokia' , 'Sony' , 'Haier'];
+
+// document.write(`<select>
+//     <option>${arr[0]}</option>
+//     <option>${arr[1]}</option>
+//     <option>${arr[2]}</option>
+//     <option>${arr[3]}</option>
+//     <option>${arr[4]}</option>
+//     <option>${arr[5]}</option>
+//     </select>`)
+
+// Home Chap (14 - 16)
+
+
+// Chp - 15
+
+
+// Q1
+
+// var arr = [];
+// console.log(arr)
+
+
+// Q2
+
+// var arr = ["Ammar"];
+// console.log(arr)
+
+
+// Q3
+
+
+// var arr = ["h" ,'i' , 'j' , 'k'];
+// alert(arr[2])
+
+
+// Q4
+
+// var arr = ['h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o'];
+// console.log(arr.length)
+
+
+// Q5
+
+
+// var arr = ['ABC'];
+// arr[1] = 'Ammar'
+// alert(arr[1])
+
+
+// Chp 16 (Array II)
+
+// Q1
+
+
+// var arr = ['Ammar']
+
+
+//  arr.push("Syed")
+
+// document.write(arr[1])
+
+
+// Q2
+
+// var arr = ['h' , 'i' , 'j' , 'k'];
+// arr.pop();
+// console.log(arr)
+
+
+// Q3
+
+// var arr = ['h' , 'i' , 'j' , 'k'];
+// arr.push(333);
+// console.log(arr);
+
+
+// Chp 16 (Array III)
+
+// var arr = ['S' , "M" , 'L' , 'XL' , 'XXL' , 'XXXL'];
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+
+
+// Q2
+
+// var arr = ['S' , "M" , 'L' , 'XL' , 'XXL' , 'XXXL'];
+// console.log(arr);
+// arr.unshift(20 , 30);
+// console.log(arr);
+
+
+// Q3
+
+// var arr = ['Ammar'];
+// arr.unshift("Syed");
+// alert(arr[0])
+
+
+// Q4
+
+
+// var arr = ['S' , "M" , 'XL' , 'XXL' , 'XXXL'];
+// console.log(arr);
+// arr.splice(2,0,"L");
+// console.log(arr);
+
+
+// Q5
+
+// var arr = ['S' , "M" , 'L' , 'XL' , 'XXL' , 'XXXL'];
+// console.log(arr);
+// var regSizes = arr.slice(0,3)
+// console.log(regSizes)
+
+
+
+// Q6
+
+// var arr = ['dog' , 'cat' , 'ox' , 'duck' , 'frog'];
+// console.log(arr);
+// arr.splice(1,3,"horse" , "Monkey")
+// console.log(arr)
+
+
+// Q7
+
+// var arr = ['dog' , 'cat' , 'ox' , 'duck' , 'frog'];
+// console.log(arr);
+// arr.splice(1,2);
+// console.log(arr)
+
+
+// Q8
+
+// var arr = ['dog' , 'cat' , 'ox' , 'duck' , 'frog' , 'flea'];
+// console.log(arr);
+// var copy = arr.slice(3,5)
+// console.log(copy);
+
